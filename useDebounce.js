86c189b9e1465857ec9327ebdf7c96861c59b1df
Abdul Ahad Rashed <maarashed40@gmail.com>
2024-03-25 01:32:10 +0600

    useDebounce.js

---
+++ useDebounce.js
@@ -0,0 +1,27 @@
+// Debounce function for react.js and next.js
+
+import { useEffect, useRef } from "react";
+
+const useDebounce = (cb, delay) => {
+  const timeoutRef = useRef();
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        return clearTimeout;
+      }
+    };
+  });
+  const debounce = (args) => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      cb(args);
+    }, delay);
+  };
+
+  return debounce;
+};
+
+export default useDebounce;
